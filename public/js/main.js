3b02ea1618dd0d1cc312c4954e12e1227fd93f54
James9446 <jameshreynolds@gmail.com>
2023-11-26 15:23:41 -0800

    Added prompt field for custom image generation. Created radio fields to select image style. Modified description prompt to detect image style and updated the frontend to automatically select the corresponding style when generating images.

--- public/js/main.js
+++ public/js/main.js
@@ -1,61 +1,137 @@
+// HANDLE CLIENT-SIDE ACTIONS
 
-function displayImage(input) {
+// handle images uploaded from local machine and generate description
+function loadImageFromFile(input) {
   if (input.files && input.files[0]) {
     var reader = new FileReader();
-    reader.onload = function (e) {
-      var image = document.getElementById('displayedImage');
-      image.src = e.target.result;
-      image.style.display = 'block';
+    reader.onload = async function (e) {
+      const base64Image = e.target.result;
+      console.log("e.target.result", base64Image);
+      displayImage('original-image', base64Image);
+
+      // Create placeholder text while description is being generated
+      updateTextElement("generating description...", 'image-description');
+    
+      // Call server to generate image description
+      const data = await generateImageDescription(base64Image)
+      updateTextElement(data.description, 'image-description');
+      updateRadioForNaturalImages(data.description);
     };
     reader.readAsDataURL(input.files[0]);
   }
-  // next step convert to base64
 }
 
+
+// handle images uploaded from url and generate description
 async function loadImageFromUrl(imageURL) {
-  let image = document.getElementById('displayedImage');
-  image.src = imageURL;
-  image.style.display = 'block';
-  updateDescription("generating description...", 'imageDescription');
+  displayImage('original-image', imageURL);
+
+  // Create placeholder text while description is being generated
+  updateTextElement("generating description...", 'image-description');
+
+  // Call server to generate image description
   const data = await generateImageDescription(imageURL)
-  // console.log(description);
-  updateDescription(data.description, 'imageDescription');
+  updateTextElement(data.description, 'image-description');
+
+  updateRadioForNaturalImages(data.description);
 }
 
-async function getImage(id, prompt) {
-  let useDescription = false;
-  if (id === 'get-image-btn') {
-    useDescription = true;
-    prompt = document.getElementById('imageDescription').textContent;
-  }
-  updateDescription("generating image...", 'image-placeholder-message');
 
-  const data = await generateImage(useDescription, prompt);
-  if (data) {
-    updateDescription("", 'image-placeholder-message');
+// handle image generation request from client
+async function getImage(id, isVivid) {
+  hideImage('generatedImage');
+
+  // determine which prompt to use
+  let prompt;
+  if (id === 'use-description-btn') {
+    prompt = document.getElementById('image-description').textContent;
+  } 
+  if (id === 'use-prompt-btn') {
+    prompt = document.getElementById('prompt-input').value;
   };
-  const imageURL = data.imageData.data[0].url;
-  const revisedPrompt = data.imageData.data[0].revised_prompt;
-  // console.log(data.imageData.data[0].revised_prompt);
-  // console.log(data.imageData.data[0].url);
-  let image = document.getElementById('generatedImage');
+
+  // determine which style to use
+  let style;
+  if (isVivid) {
+    style = 'vivid';
+  } else {
+    style = 'natural';
+  };
+  
+  // show placeholder message
+  updateTextElement("generating image...", 'image-placeholder-message');
+
+  // Call server to generate image
+  const requestBody = { prompt, style };
+  const data = await generateImage(requestBody);
+
+  // hide the placeholder message
+  if (data) {
+    updateTextElement("", 'image-placeholder-message');
+  };
+  const imageURL = data.imageData.url;
+  const revisedPrompt = data.imageData.revised_prompt;
+  displayImage('generatedImage', imageURL);
+  updateTextElement(revisedPrompt, 'revised-prompt');
+}
+
+
+
+
+// CLIENT-SIDE UTILITY FUNCTIONS
+function displayImage(id, imageURL) {
+  let image = document.getElementById(id);
   image.src = imageURL;
   image.style.display = 'block';
-  updateDescription(revisedPrompt, 'revisedPrompt');
 }
 
-function updateDescription(text, id) {
+function hideImage(id) {
+  let image = document.getElementById(id);
+  image.style.display = 'none';
+}
+
+function updateTextElement(text, id) {
   document.getElementById(id).textContent = text;
 }
 
-const copyToClipboard = (id) => {
+function copyToClipboard(id) {
   console.log("id", id);
   let text = document.getElementById(id).textContent;
   navigator.clipboard.writeText(text);
 };
 
-async function generateImageDescription(imageURL) {
+function downloadImage(id) {
+  // var image = document.getElementById(id);
+  //   var imageUrl = image.getAttribute('src');
+  //   var imageName = imageUrl.substring(imageUrl.lastIndexOf('/') + 1);
+  //   var link = document.createElement('a');
+  //   link.href = imageUrl;
+  //   link.download = imageName;
+  //   document.body.appendChild(link);
+  //   link.click();
+  //   document.body.removeChild(link);
+  // let image = document.getElementById(id);
+  // let link = document.createElement('a');
+  // link.href = image.src;
+  // link.download = 'image.png';
+  // link.click();
+};
 
+// DATA UTILITY FUNCTIONS
+function updateRadioForNaturalImages(imageDescription) {
+  const isNatural = imageDescription.includes("'natural'");
+  if (isNatural) {
+    const naturalRadio = document.getElementById('natural-radio')
+    naturalRadio.checked = true;
+  }
+}
+
+
+
+
+
+// BACKEND REQUESTS
+async function generateImageDescription(imageURL) {
   try {
     const response = await fetch('/vision', {
       method: 'POST',
@@ -72,21 +148,17 @@ async function generateImageDescription(imageURL) {
   }
 }
 
-async function generateImage(useDescription=true, prompt) {
-  if (useDescription) {
-    prompt = document.getElementById('imageDescription').textContent;
-  }
+async function generateImage(requestBody) {
   try {
     const response = await fetch('/dall-e', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({prompt}),
+      body: JSON.stringify(requestBody),
     });
 
     const data = await response.json();
-    // console.log("image data: ", data);
     return data;
   } catch (error) {
     console.error('Error:', error);
--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+// base64 images require a larger limit
+app.use(bodyParser.json({ limit: '10mb' }));
 
 app.use(express.static(path.join(__dirname, '../public')))
 
@@ -26,7 +28,7 @@ app.post('/vision', async (req, res) => {
         "content": [
           {
             "type": "text", 
-            "text": "Describe what’s in this image. Inlcude style, composition, colors, shapes, textures, lighting and any other details that would help someone understand what’s in the image."
+            "text": "Describe what’s in this image. Inlcude style, composition, colors, shapes, textures, lighting and any other details that would help someone understand what’s in the image. At the end of the descrption always provide a imageType catorization where the image is categorized as either 'vivid' or 'natural', vivid means the image looks hyper-real, while natural means the image looks less hyper-real."
           },
           {
             "type": "image_url",
@@ -43,8 +45,8 @@ app.post('/vision', async (req, res) => {
     // Make a POST request to the API endpoint
     const apiResponse = await axios.post(completionsURL, visionBody, {
       headers: {
-        'Authorization': `Bearer ${key}`, // Include any necessary headers (e.g., authorization)
-        'Content-Type': 'application/json' // Adjust content type based on API requirements
+        'Authorization': `Bearer ${key}`, 
+        'Content-Type': 'application/json' 
       }
     });
 
@@ -64,7 +66,8 @@ app.post('/vision', async (req, res) => {
 
 // Genrate an image 
 app.post('/dall-e', async (req, res) => {
-  const {prompt} = req.body;
+  console.log(req.body);
+  const {prompt, style} = req.body;
   const key = process.env.OPEN_API_KEY;
   const imageGenerationURL = 'https://api.openai.com/v1/images/generations'; 
   const imageGenerationBody = {
@@ -72,24 +75,19 @@ app.post('/dall-e', async (req, res) => {
     "prompt": prompt,
     "n": 1,
     "size": "1024x1024",
-    "style": "vivid",
+    "style": style,
     "response_format": "url"
   };
   try {
     // Make a POST request to the API endpoint
     const apiResponse = await axios.post(imageGenerationURL, imageGenerationBody, {
       headers: {
-        'Authorization': `Bearer ${key}`, // Include any necessary headers (e.g., authorization)
-        'Content-Type': 'application/json' // Adjust content type based on API requirements
+        'Authorization': `Bearer ${key}`, 
+        'Content-Type': 'application/json' 
       }
     });
 
-    const imageData = await apiResponse.data;
-
-    console.log(imageData);
-
-    // console.log(JSON.stringify(apiResponse.data));
-    // Handle the API response as needed
+    const imageData = await apiResponse.data.data[0];
     res.status(200).json({imageData});
   } catch (error) {
     // Handle errors if the API request fails
