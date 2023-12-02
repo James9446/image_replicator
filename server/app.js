6b3fe109b08aa88cf37017145a677605a68d2f4f
James9446 <jameshreynolds@gmail.com>
2023-12-01 18:46:53 -0800

    Modified the About page

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // base64 images require a larger limit
 app.use(bodyParser.json({ limit: '10mb' }));
